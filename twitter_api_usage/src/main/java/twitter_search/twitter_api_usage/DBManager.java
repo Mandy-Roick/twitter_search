0bfb5c9ea76e4588c0c4d5eb2ff4b92d8bcb03d9
Mandy Roick <mandy.roick@student.hpi.uni-potsdam.de>
2014-07-20 14:35:09 +0200

    Fix bugs and get DBManager running
    now need to make url connection parallel

--- twitter_api_usage/src/main/java/twitter_search/twitter_api_usage/DBManager.java
+++ twitter_api_usage/src/main/java/twitter_search/twitter_api_usage/DBManager.java
@@ -1,9 +1,18 @@
 package twitter_search.twitter_api_usage;
 
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import twitter4j.HashtagEntity;
 import twitter4j.Status;
+import twitter4j.URLEntity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.sql.*;
-import org.postgis.*;
 
 /**
  * Created by Mandy Roick on 14.07.2014. according to example by Maximilian Jenders
@@ -11,8 +20,8 @@ import org.postgis.*;
 public class DBManager {
     private Connection connection;
 
-    private String password = "";
-    private String user = "";
+    private String password = "9lBZzYDpVDEGjhOPysab";
+    private String user = "roick";
 
     public DBManager() {
         connect();
@@ -40,7 +49,104 @@ public class DBManager {
             createTweet(tweet);
         }
 
-        //TODO: add Hashtags and url content
+        writeHashtagsToDB(tweet);
+        writeUrlContentToDB(tweet);
+    }
+
+    private void writeHashtagsToDB(Status tweet) {
+        HashtagEntity[] hashtags = tweet.getHashtagEntities();
+        for (HashtagEntity hashtag : hashtags) {
+            if (!hashtagForTweetDoesExist(tweet.getId(), hashtag.getText())) {
+                createHashtagForTweet(tweet, hashtag);
+            }
+        }
+    }
+
+    private boolean hashtagForTweetDoesExist(long tweetId, String hashtagText) {
+        boolean doesExist = false;
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM mandy_masterarbeit.twitter_tweet_hashtag WHERE (hashtag = '" +
+                    hashtagText + "') AND (tweet = " + tweetId + ")");
+
+            if(result.next()) {
+                doesExist = true;
+            }
+
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return doesExist;
+    }
+
+    private void createHashtagForTweet(Status tweet, HashtagEntity hashtag) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("" +
+                    "INSERT INTO mandy_masterarbeit.twitter_tweet_hashtag(hashtag, tweet)" +
+                    "VALUES (?, ?)");
+            statement.setString(1, hashtag.getText());
+            statement.setLong(2, tweet.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeUrlContentToDB(Status tweet) {
+        URLEntity[] urls = tweet.getURLEntities();
+        URL url;
+        for (URLEntity urlEntitiy : urls) {
+            if(!urlForTweetDoesExist(tweet.getId(), urlEntitiy.getText())) {
+                createUrlForTweet(tweet, urlEntitiy);
+            }
+        }
+    }
+
+    private boolean urlForTweetDoesExist(long tweetId, String url) {
+        boolean doesExist = false;
+        try {
+            Statement statement = this.connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM mandy_masterarbeit.twitter_tweet_url WHERE (url = '" +
+                    url + "') AND (tweet = " + tweetId + ")");
+
+            if(result.next()) {
+                doesExist = true;
+            }
+
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return doesExist;
+    }
+
+    private void createUrlForTweet(Status tweet, URLEntity urlEntitiy) {
+        PreparedStatement statement = null;
+        try {
+            URL url = new URL(urlEntitiy.getURL());
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String urlText = "";
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                urlText += inputLine;
+            }
+            br.close();
+            statement = this.connection.prepareStatement("" +
+                    "INSERT INTO mandy_masterarbeit.twitter_tweet_url(url, content, tweet)" +
+                    "VALUES (?, ?, ?)");
+            statement.setString(1, urlEntitiy.getText());
+            statement.setString(2, urlText);
+            statement.setLong(3, tweet.getId());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void createTweet(Status tweet) {
