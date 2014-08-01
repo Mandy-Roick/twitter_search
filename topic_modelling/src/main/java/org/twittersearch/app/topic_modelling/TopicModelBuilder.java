3c17f5c990b2b302bda5c149dba46cc97d89fe90
Mandy Roick <mandy.roick@student.hpi.uni-potsdam.de>
2014-08-01 11:15:41 +0200

    Write results to file in TopicModelBuilder

--- topic_modelling/src/main/java/org/twittersearch/app/topic_modelling/TopicModelBuilder.java
+++ topic_modelling/src/main/java/org/twittersearch/app/topic_modelling/TopicModelBuilder.java
@@ -1,5 +1,6 @@
 package org.twittersearch.app.topic_modelling;
 
+import au.com.bytecode.opencsv.CSVWriter;
 import cc.mallet.pipe.*;
 import cc.mallet.pipe.iterator.CsvIterator;
 import cc.mallet.topics.ParallelTopicModel;
@@ -29,38 +30,57 @@ public class TopicModelBuilder {
 
         // Pipes: lowercase, tokenize, remove stopwords, map to features
         pipeList.add(new CharSequenceLowercase());
-        pipeList.add(new CharSequence2TokenSequence(Pattern.compile("\\p{L}[\\p{L}\\p{Pc}]+\\p{L}")));
+        pipeList.add(new CharSequence2TokenSequence(Pattern.compile("[\\p{L}][\\p{L}\\p{Pd}\\p{M}]+\\p{L}")));
+        //This pattern filters all sequences of at least 3 literals (also in urls, in users, ...)
+        //TODO: ignore users, urls, ...
         pipeList.add(new TokenSequenceRemoveStopwords(new File("stop_lists/stop_words_mysql.txt"), "UTF-8", false, false, false));
         pipeList.add(new TokenSequence2FeatureSequence());
 
         InstanceList instances = new InstanceList (new SerialPipes(pipeList));
 
+        int numTopics = 10;
+        String dateSuffix = "_2014-07-30";
+        String filePrefix = "tm-" + numTopics + dateSuffix;
+
         Reader fileReader = null;
         try {
-            fileReader = new InputStreamReader(new FileInputStream(new File("mallet_input_file.csv")), "UTF-8");
+            fileReader = new InputStreamReader(new FileInputStream(new File("mallet_input_file" + dateSuffix + ".csv")), "UTF-8");
             instances.addThruPipe(new CsvIterator(fileReader, Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)"),
                     3, 2, 1)); // data, label, name fields
 
-            int numTopics = 10;
             ParallelTopicModel model = new ParallelTopicModel(numTopics, 1.0, 0.01);
             model.addInstances(instances);
             model.setNumThreads(2);
             model.setNumIterations(500);
             model.estimate();
 
-            model.printDocumentTopics(new File("tm-10_2014-07-24.results"));
+            //model.printTopicWordWeights(new File(filePrefix + "words.results"));
+            //model.printTypeTopicCounts(new File(filePrefix + "type_topic_counts.results"));
+            //model.displayTopWords(10,true);
 
+            CSVWriter topWordsCsvWriter = new CSVWriter(new FileWriter(filePrefix + "_top_words.results"), ',', ' ');
+            Object[][] topWords = model.getTopWords(20);
+            for (Object[] topic : topWords) {
+                String[] line = new String[topic.length];
+                for (int i = 0; i < topic.length; i++) {
+                    line[i] = (String) topic[i];
+                }
+                topWordsCsvWriter.writeNext(line);
+            }
+            topWordsCsvWriter.close();
             // The data alphabet maps word IDs to strings
-            //Alphabet dataAlphabet = instances.getDataAlphabet();
+            Alphabet dataAlphabet = instances.getDataAlphabet();
+            //System.out.println("dataAlphabet:");
+            //System.out.println(dataAlphabet.toString());
 
-            //FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance.getData();
-            //LabelSequence topics = model.getData().get(0).topicSequence;
+            FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance.getData();
+            LabelSequence topics = model.getData().get(0).topicSequence;
 
-            //Formatter out = new Formatter(new StringBuilder(), Locale.US);
-            //for (int position = 0; position < tokens.getLength(); position++) {
-            //    out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)), topics.getIndexAtPosition(position));
-            //}
-            //System.out.println(out);
+            Formatter out = new Formatter(new StringBuilder(), Locale.US);
+            for (int position = 0; position < tokens.getLength(); position++) {
+                out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)), topics.getIndexAtPosition(position));
+            }
+            System.out.println(out);
 
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
