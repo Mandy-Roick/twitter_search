5bbc6b919383a7f3f3177f8efa9b95426a1cb2e0
Mandy Roick <mandy.roick@student.hpi.uni-potsdam.de>
2014-08-20 13:12:52 +0200

    Implement the search for tweets in the database
    whose urls have not been saved,
    but does not work properly because of constraints from the twitter API

---
+++ twitter_api_usage/src/main/java/org/twittersearch/app/twitter_api_usage/TweetUrlChecker.java
@@ -0,0 +1,57 @@
+package org.twittersearch.app.twitter_api_usage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Mandy Roick on 18.08.2014.
+ */
+
+// Not working because of Twitter's Rate Limit
+public class TweetUrlChecker {
+
+    public static void main(String[] args) {
+        collectAndWriteMissingUrlsToDB("2014-07-24");
+    }
+
+    public static void collectAndWriteMissingUrlsToDB(String date) {
+        DBManager dbManager = new DBManager();
+        System.out.println("Collect missing Urls.");
+        Map<Long, List<String>> tweetsUrls = collectMissingUrls(dbManager, date);
+        System.out.println("Write missing Urls to DB.");
+        writeMissingUrlsToDB(tweetsUrls, dbManager);
+    }
+
+    private static Map<Long, List<String>> collectMissingUrls(DBManager dbManager, String date) {
+        TwitterManager twitterManager = new TwitterManager();
+        Map<Long, List<String>> tweetsUrls = new HashMap<Long, List<String>>();
+
+        int counter = 0;
+        Map<Long,String> tweets = dbManager.selectTweetsCreatedAt(date);
+        System.out.println("Have to collect " + tweets.size() + " Tweets.");
+        for(Long tweetId : tweets.keySet()) {
+            System.out.println(counter);
+            List<String> urls = twitterManager.getUrlsForTweet(tweetId);
+            tweetsUrls.put(tweetId,urls);
+
+            counter++;
+        }
+
+        return tweetsUrls;
+    }
+
+    private static void writeMissingUrlsToDB(Map<Long, List<String>> tweetsUrls, DBManager dbManager) {
+        int counter = 0;
+        for(Map.Entry<Long, List<String>> tweetUrls : tweetsUrls.entrySet()) {
+            for(String url : tweetUrls.getValue()) {
+                dbManager.writeUrlContentToDB(tweetUrls.getKey(), url, false);
+            }
+
+            if ((counter % 100) == 0) {
+                System.out.println(counter + "Tweets are done.");
+            }
+            counter++;
+        }
+    }
+}
