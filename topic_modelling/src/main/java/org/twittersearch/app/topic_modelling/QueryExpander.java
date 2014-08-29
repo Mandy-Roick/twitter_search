b1487b485334bd9defdf77c815ff6ba7caaeb4c9
Mandy Roick <mandy.roick@student.hpi.uni-potsdam.de>
2014-08-29 14:00:28 +0200

    Refactor Query Expander to be more flexible
    in number of topics per query element and top words per topic

--- topic_modelling/src/main/java/org/twittersearch/app/topic_modelling/QueryExpander.java
+++ topic_modelling/src/main/java/org/twittersearch/app/topic_modelling/QueryExpander.java
@@ -13,13 +13,15 @@ import java.util.regex.Pattern;
  * Created by Mandy Roick on 20.08.2014.
  */
 public class QueryExpander {
+    private static int numOfTopicsForExpansion = 3;
+    private static int numOfTopWordsPerTopic = 3;
 
     public static void main(String[] args) {
         String[] expandedQuery;
         if (args.length == 1 ) {
             expandedQuery = expand(args[1]);
         } else {
-            expandedQuery = expand("Warum #Neuland ein schönes Meme, aber eigentlich gar nicht lustig ist http://s.gilly.cat/12bQ6sj");
+            expandedQuery = expand("politics");
         }
 
         for (String queryElement : expandedQuery) {
@@ -33,9 +35,63 @@ public class QueryExpander {
         String[] splitQuery = splitQuery(preprocessedQuery);
         String[] postprocessedQuery = postprocessQuery(splitQuery);
 
-        Map<String, String[]> typeTopicCounts = readTopicModel("");
-        
-        return postprocessedQuery;
+        Map<String, String[]> typeTopicCounts = readTopicModel("trimmed_tm-50_2014-08-02_type_topic_counts.results");
+        Map<Integer, String[]> topWords = readTopWords("trimmed_tm-50_2014-08-02_top_words.results");
+
+        String[] expandedQuery = expandThroughTopicModel(postprocessedQuery, typeTopicCounts, topWords);
+
+        return expandedQuery;
+    }
+
+    private static String[] expandThroughTopicModel(String[] query, Map<String, String[]> typeTopicCounts, Map<Integer, String[]> topWords) {
+        List<String> expandedQuery = new ArrayList<String>();
+
+        for (String queryElement : query) {
+            expandedQuery.add(queryElement);
+
+            String[] topicCounts = typeTopicCounts.get(queryElement);
+            if (topicCounts != null) {
+
+                String topicCount;
+                String[] topicIndexString;
+                String[] topWordsForTopic;
+                for (int i = 0; (i < topicCounts.length) && (i < numOfTopicsForExpansion); i++) {
+                    topicCount = topicCounts[i];
+                    topicIndexString = topicCount.split(":");
+                    int topicIndex = Integer.parseInt(topicIndexString[0]);
+                    topWordsForTopic = topWords.get(topicIndex);
+                    for (int j = 0; (j < topWordsForTopic.length) && (j < numOfTopWordsPerTopic); j++) {
+                        expandedQuery.add(topWordsForTopic[j]);
+                    }
+                    expandedQuery.add("");
+                }
+
+            }
+        }
+        return expandedQuery.toArray(new String[expandedQuery.size()]);
+    }
+
+    private static Map<Integer, String[]> readTopWords(String fileName) {
+        Map<Integer, String[]> topWords = new HashMap<Integer, String[]>();
+
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(fileName), ',', ' ');
+            int topicIndex = 0;
+            String[] nextLine;
+            while ((nextLine = csvReader.readNext()) != null) {
+                String[] topWordsForTopic = Arrays.copyOfRange(nextLine, 3, nextLine.length);
+                topWords.put(Integer.parseInt(nextLine[0]), topWordsForTopic);
+                topicIndex++;
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open Topic File.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Could not read next line in Topic File.");
+            e.printStackTrace();
+        }
+        return topWords;
     }
 
     private static Map<String, String[]> readTopicModel(String fileName) {
@@ -55,7 +111,7 @@ public class QueryExpander {
             System.out.println("Could not read next line in Topic File.");
             e.printStackTrace();
         }
-        return null;
+        return typeTopicCounts;
     }
 
     private static String[] postprocessQuery(String[] splitQuery) {
